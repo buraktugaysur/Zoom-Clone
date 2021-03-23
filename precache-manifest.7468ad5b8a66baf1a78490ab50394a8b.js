29d4cec115c9f29acd38dc5514a076f7d060994c
Burak Tugay SUR <buraktugaysur@gmail.com>
2021-03-23 20:43:08 +0300

    Upload to version 1.0.0

---
+++ precache-manifest.7468ad5b8a66baf1a78490ab50394a8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9eef75f497f02b5111f0581e15d03b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1405ef09f42f16330634",
+    "url": "/static/css/2.6ab4978e.chunk.css"
+  },
+  {
+    "revision": "453c846473b614fd76f7",
+    "url": "/static/css/main.0648502a.chunk.css"
+  },
+  {
+    "revision": "1405ef09f42f16330634",
+    "url": "/static/js/2.6e9c4bac.chunk.js"
+  },
+  {
+    "revision": "71e9e4dedda4f381ea56e80067d4fca8",
+    "url": "/static/js/2.6e9c4bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "453c846473b614fd76f7",
+    "url": "/static/js/main.fa5d7f5a.chunk.js"
+  },
+  {
+    "revision": "5ff6df50c1a535018958",
+    "url": "/static/js/runtime-main.df338948.js"
+  }
+]);
